35e274d59c50f534abd39979556247d7c8ec2cd2
Ahmedkhaledd1 <ahmedkhaledno1@gmail.com>
2024-04-28 18:40:56 +0300

    still in progress

--- gui.py
+++ gui.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template,request
+import os
+from worker_thread import uploadImage
+
 
 
 main_window = Flask(__name__)
+main_window.config["upload-path"]="static/uploadFolder"
 
 @main_window.route("/")
 def HomePage():
@@ -13,6 +17,14 @@ def HomePage():
 def image_process():
     image=request.files["uploaded_image"]
 
+    if image:
+        image.save(os.path.join(main_window.config["upload-path"], image.filename))
+        # Get the path of the saved image
+        image_path = os.path.join(main_window.config["upload-path"], image.filename)
+        print(image_path)
+        return render_template('homepage.html', image_path=image_path)
+    else:
+        return render_template('homepage.html', image_path=None)
 
 
 
---
+++ worker_thread.py
@@ -0,0 +1,60 @@
+import threading
+import queue
+import cv2  # OpenCV for image processing
+from mpi4py import MPI  # MPI for distributed computing
+import numpy as np
+import matplotlib.pyplot as plt
+
+class WorkerThread(threading.Thread):
+    def __init__(self, task_queue):
+        threading.Thread.__init__(self)
+        self.task_queue = task_queue
+        self.comm = MPI.COMM_WORLD
+        self.rank = self.comm.Get_rank()
+        self.result = None  # Variable to store processed result
+
+    def run(self):
+        while True:
+            task = self.task_queue.get()
+            if task is None:
+                break
+
+            image, operation = task
+            result = self.process_image(image, operation)
+            self.comm.send(result, dest=0)
+
+    def process_image(self, image, operation):
+        # Load the image
+        img = cv2.imread(image, cv2.IMREAD_COLOR)
+
+        # Perform the specified operation
+        if operation == 'edge_detection':
+            result = cv2.Canny(img, 100, 200)
+        elif operation == 'color_inversion':
+            result = cv2.bitwise_not(img)  # Add more operations as needed...
+        
+        return result
+
+
+
+
+def uploadImage (image,op):
+    task_queue = queue.Queue()
+
+    # Instantiate WorkerThread
+    worker_thread = WorkerThread(task_queue)
+
+    # Add image processing task to the queue
+    image_path = image
+    operation =op  # Specify the desired operation
+    task_queue.put((image_path, operation))
+
+    # Start WorkerThread
+    worker_thread.start()
+
+
+    if MPI.COMM_WORLD.Get_rank() == 0:  # Only master node receives result
+        processed_result = MPI.COMM_WORLD.recv(source=0)
+        return processed_result
+    else:
+        return None

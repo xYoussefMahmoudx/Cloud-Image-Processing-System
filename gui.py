8caca68abaf10bd0458f128b82aee08b6fe094ff
Ahmedkhaledd1 <ahmedkhaledno1@gmail.com>
2024-05-09 20:02:45 +0300

    GUI Done

--- gui.py
+++ gui.py
@@ -1,8 +1,16 @@
+import base64
+
 from flask import Flask, render_template,request
 import os
+
+from matplotlib import pyplot as plt
 from worker_thread import processImage
 import cv2
 import shutil
+# import rpyc
+
+# conn = rpyc.connect("localhost", 18861)  # Connect to the RPyC server running on localhost
+
 
 
 
@@ -51,22 +59,29 @@ def image_process():
     selected_option = request.form.get("selected_option")
     print(selected_option)
     image_path = get_latest_uploaded_file()
-    image_path= shutil.copy(image_path,"static\downloadFolder")
-    print(image_path)
-    image = cv2.imread(image_path, cv2.IMREAD_COLOR)
-    result = processImage(image_path,selected_option)
-    # result = cv2.Canny(image_path, 100, 200)
-    cv2.imwrite(image_path, result)
-
     
 
-    return render_template('homepage.html', image_path=image_path)
+    image_path= shutil.copy(image_path,"static\downloadFolder")
+    result=cv2.imread(image_path, cv2.IMREAD_COLOR )
+
+
+    result = processImage(result ,selected_option)
+    # result = cv2.Canny(image_path, 100, 200)
+    # cv2.imwrite(image_path, result)
+    succ,encoded_image = cv2.imencode('.jpeg', result)
+    
+    print(encoded_image)
+
+    
+    encoded_image = base64.b64encode(encoded_image).decode('utf-8')
+    return render_template('homepage.html', encoded_image=encoded_image)
+    # return render_template('homepage.html', image_path)
 
 
 
 
 
 if __name__ == "__main__":
-    main_window.run(debug=True,port=5000)
+    main_window.run(debug=True, host='192.168.80.128', port=5000)
 
 
--- worker_thread.py
+++ worker_thread.py
@@ -25,13 +25,13 @@ class WorkerThread(threading.Thread):
 
     def process_image(self, image, operation):
         # Load the image
-        img = cv2.imread(image, cv2.IMREAD_COLOR)
+        # img = cv2.imread(image, cv2.IMREAD_COLOR )
 
         # Perform the specified operation
         if operation == 'edge_detection':
-            result = cv2.Canny(img, 100, 200)
+            result = cv2.Canny(image, 100, 200)
         elif operation == 'color_inversion':
-            result = cv2.bitwise_not(img)  # Add more operations as needed...
+            result = cv2.bitwise_not(image)  # Add more operations as needed...
         
         return result
 
